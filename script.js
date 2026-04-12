8cdef33b608b90111763e28d89a0fdcc7dd4d69a
Arjay Dimaunahan <arjay213112@gmail.com>
2026-04-12 20:44:52 +0800

    feat: add book rendering and toggle read functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const bookForm = document.getElementById('book-form');
 const showDocument = document.querySelector('#new-book-btn');
+const libraryContainer = document.querySelector('#library-container');
 
 const myLibrary = [];
 
@@ -23,10 +24,39 @@ function addBookToLibrary(){
     myLibrary.push(newBook);
 
     console.log(myLibrary);
+    renderLibrary()
+
 }
 
+function renderLibrary(){
+    libraryContainer.innerHTML = "";
+    myLibrary.forEach((book, index)=>{
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
 
+        bookDiv.innerHTML = `
+        <h3> ${book.title}</h3>
+        <p>${book.author} • ${book.page}</p>
+        <p>Status: ${book.isRead ? "Read" : "Not Read"}</p>
+        <div class="book-action">
+        <button class="toggle-read" data-index="${index}">Mark read</button>
+        <button class="remove" data-index="${index}">Remove</button>
+        </div>
+        `
 
+        libraryContainer.appendChild(bookDiv)
+    })
+}
+
+libraryContainer.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("toggle-read")){
+         const index = e.target.dataset.index;
+
+        myLibrary[index].isRead = !myLibrary[index].isRead;
+
+        renderLibrary();
+    }
+})
 
 
 bookForm.addEventListener('submit', (event)=>{
